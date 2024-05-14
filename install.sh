2e59a1b44a86984cbf56146a11ef28956fceadea
newbie <72300646+spare-proxy@users.noreply.github.com>
2024-05-14 18:38:03 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,9 @@
-GREEN="\e[32m"
-ENDCOLOR="\e[0m"
-
 pkg update -y
 pkg upgrade -y
 pkg install openssl -y
 pkg install curl -y
 pkg install libenet -y
 
-echo -e "${GREEN}Installing Spare Premium Proxy...${ENDCOLOR}"
+echo -e installing Spare Premium Proxy...
 wget -q https://github.com/spare-proxy/android/raw/main/proxy_linux
-
-
 chmod +x proxy_linux
